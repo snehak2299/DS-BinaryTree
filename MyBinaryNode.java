64fc596839fece8eaeeaaf37b10b3f3aba5c24a2
snehak2299 <skanase2299@gmail.com>
2021-08-26 13:10:46 +0530

    [sneha]:basic setup added

---
+++ MyBinaryNode.java
@@ -0,0 +1,5 @@
+package com.bridglab.binarytree;
+
+public class MyBinaryNode {
+
+}
---
+++ MyBinaryTreeRunner.java
@@ -0,0 +1,10 @@
+package com.bridglab.binarytree;
+
+public class MyBinaryTreeRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
