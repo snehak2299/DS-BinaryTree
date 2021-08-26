372125cd3bd73fd70d15c53c5f57d807dd631a13
snehak2299 <skanase2299@gmail.com>
2021-08-26 20:43:04 +0530

    [sneha]:ability to add multiple node

--- MyBinaryTreeRunner.java
+++ MyBinaryTreeRunner.java
@@ -1,14 +1,21 @@
 package com.bridglab.binarytree;
 
+import java.util.Scanner;
+
 import com.bridglab.linkedlist.MyNode;
 
 public class MyBinaryTreeRunner {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
-		myBinaryTree.add(56);
-		myBinaryTree.add(30);
-		myBinaryTree.add(70);
+		System.out.println("how many nodes you want to add");
+		int count = sc.nextInt();
+		for(int i=0;i<=count-1;i++) {
+		System.out.println("Enter nodes to added in binary tree");
+		myBinaryTree.add(sc.nextInt());
+		}
+		count+=1;
 		int size=myBinaryTree.getSize();
 		System.out.println(size);
 
